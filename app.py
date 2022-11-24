9efdbbf6499ac23e259565740cc59f418690dc83
Luke <shijiluke@gmail.com>
2022-11-24 12:53:10 +0800

    2nd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World fuk dis dat sht!'
 
 
 if __name__ == '__main__':
